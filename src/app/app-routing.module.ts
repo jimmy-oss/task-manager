882eb7bef445b0e7e6df38eb4a00ab9b4b407f10
jimmy-oss <jimmynjonge51@gmail.com>
2022-10-15 14:59:56 +0300

    routing the application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"dashboard",component:DashboardComponent},
+  {path:"about",component:AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
