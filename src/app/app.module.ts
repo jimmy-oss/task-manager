8cf27c3f545a53fa96cb430f91748dafa9cca0a6
jimmy-oss <jimmynjonge51@gmail.com>
2022-10-15 14:06:21 +0300

    invocking the app components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
